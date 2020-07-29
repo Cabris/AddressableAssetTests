a8f9374013fd6edabc8bd48bfd1c33881c805cf2
Christ Chen <christ_chen@htc.com>
2020-07-29 16:30:42 +0800

    builded bundle has own subfolder

--- AddressableAssetBuilder/Assets/Scripts/Editor/BuildAddressablesProcessor.cs
+++ AddressableAssetBuilder/Assets/Scripts/Editor/BuildAddressablesProcessor.cs
@@ -2,33 +2,46 @@
 using UnityEditor.AddressableAssets;
 using UnityEditor.AddressableAssets.Settings;
 using UnityEngine;
-using System.Collections;
 using System.IO;
-using UnityEditor.Build;
-using System.Collections.Generic;
-using System.Linq;
-using System;
 
-class BuildAddressablesProcessor
+
+namespace WTC
 {
+    public static class AddressablesConstants
+    {
+        public static string BundleRootID = "DataRoot";
+        public static string SettingPath = "Assets/AddressableAssetsData/AddressableAssetSettings.asset";
+
+    }
+
+}
+
+static class BuildAddressablesProcessor
+{
+
     [MenuItem("Assets/AddressableAsset/BuildBundles with Prefab")]
     static void BuildBundlesWithPrefab()
     {
         var target = Selection.activeObject;
         PrefabAssetType type = PrefabUtility.GetPrefabAssetType(target);
-        //Debug.Log("BuildBundles with prefab, target type: " + type);
+
         if (type == PrefabAssetType.NotAPrefab)
         {
             Debug.LogError("select object must be a prefab!!");
             return;
         }
 
-        string path = AssetDatabase.GetAssetPath(target);
-        //Debug.Log("path: " + path);
-        string guid = AssetDatabase.AssetPathToGUID(path);
+        if (type == PrefabAssetType.MissingAsset)
+        {
+            Debug.LogError("select object MissingAsset!");
+            return;
+        }
 
-        string settingPath = "Assets/AddressableAssetsData/AddressableAssetSettings.asset";
-        var settings = AssetDatabase.LoadAssetAtPath<AddressableAssetSettings>(settingPath);
+        string assetPath = AssetDatabase.GetAssetPath(target);
+        //Debug.Log("path: " + path);
+        string guid = AssetDatabase.AssetPathToGUID(assetPath);
+
+        AddressableAssetSettings settings = AssetDatabase.LoadAssetAtPath<AddressableAssetSettings>(WTC.AddressablesConstants.SettingPath);
 
         var groups = settings.groups.ToArray();
         foreach (var g in groups)
@@ -44,18 +57,52 @@ class BuildAddressablesProcessor
 
         var entry = settings.CreateOrMoveEntry(guid, group);
         var address = entry.address;
+        var entryName = address.Replace("/", "_");
 
         SwitchWebGL();
-        var webGlJson = CleanAndBuildAssets();
+        var webGlJson = CleanAndBuildAssets(settings);
         var remoteLoadPath = settings.RemoteCatalogLoadPath.GetValue(settings);
         Debug.Log("SwitchWebGL remoteLoadPath: " + remoteLoadPath);
-        webGlJson = remoteLoadPath + "/" + webGlJson;
+        webGlJson = remoteLoadPath + "/" + entryName + "/" + BuildTarget.WebGL.ToString() + "/" + webGlJson;
+
 
         SwitchAndroid();
-        var androidJson = CleanAndBuildAssets();
+        var androidJson = CleanAndBuildAssets(settings);
         remoteLoadPath = settings.RemoteCatalogLoadPath.GetValue(settings);
         Debug.Log("SwitchAndroid remoteLoadPath: " + remoteLoadPath);
-        androidJson = remoteLoadPath + "/" + androidJson;
+        androidJson = remoteLoadPath + "/" + entryName + "/" + BuildTarget.Android.ToString() + "/" + androidJson;
+
+
+        var profile = settings.profileSettings;
+        var profileId = settings.activeProfileId;
+        var DataRoot = profile.GetValueByName(profileId, WTC.AddressablesConstants.BundleRootID);
+
+        string dataPath = Application.dataPath;
+        var fullPath = dataPath.Replace("/Assets", "") + "/" + DataRoot;
+
+        var entryPath = fullPath + "/" + entryName;
+
+        if (Directory.Exists(entryPath))
+        {
+            Directory.Delete(entryPath, true);
+        }
+        Directory.CreateDirectory(entryPath);
+
+        var sourcePath = fullPath + "/" + BuildTarget.WebGL.ToString();
+        if (Directory.Exists(sourcePath))
+        {
+            var targetPath = entryPath + "/" + BuildTarget.WebGL.ToString();
+            Directory.Move(sourcePath, targetPath);
+            Debug.Log("Directory.Move: " + sourcePath + " to " + targetPath);
+        }
+
+        sourcePath = fullPath + "/" + BuildTarget.Android.ToString();
+        if (Directory.Exists(sourcePath))
+        {
+            var targetPath = entryPath + "/" + BuildTarget.Android.ToString();
+            Directory.Move(sourcePath, targetPath);
+            Debug.Log("Directory.Move: " + sourcePath + " to " + targetPath);
+        }
 
 
         AddressableAssetsConfigs configs = new AddressableAssetsConfigs();
@@ -65,8 +112,13 @@ class BuildAddressablesProcessor
 
         string jsonStr = JsonUtility.ToJson(configs);
 
-        string assetPath = Application.dataPath;
-        var indexPath = assetPath.Replace("/Assets", "") + "/ServerData/Indexes/" + address.Replace("/", "_") + ".json";
+
+        string folderPath = assetPath.Replace("/Assets", "") + "/" + DataRoot + "/Indexes/"; // your code goes here
+        bool exists = Directory.Exists(folderPath);
+        if (!exists)
+            Directory.CreateDirectory(folderPath);
+
+        var indexPath = folderPath + entryName + ".json";
         Debug.Log("write json data path: " + indexPath);
 
         using (StreamWriter file =
@@ -78,61 +130,21 @@ class BuildAddressablesProcessor
 
     }
 
-    [MenuItem("Switch Platform/WebGL")]
-    static void SwitchWebGL()
+
+    static private string CleanAndBuildAssets(AddressableAssetSettings settings)
     {
-        // Switch to WebGLbuild.
-        EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup.WebGL, BuildTarget.WebGL);
-    }
 
-    [MenuItem("Switch Platform/Android")]
-    static void SwitchAndroid()
-    {
-        // Switch to WebGLbuild.
-        EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup.Android, BuildTarget.Android);
-    }
+        var profile = settings.profileSettings;
+        var profileId = settings.activeProfileId;
+        var DataRoot = profile.GetValueByName(profileId, WTC.AddressablesConstants.BundleRootID);
+        Debug.Log("DataRoot: " + DataRoot);
 
-    /*[MenuItem("AddressableAsset/BuildBundles")]
-    static private void BuildBundles()
-    {
-        string settingPath = "Assets/AddressableAssetsData/AddressableAssetSettings.asset";
-        AddressableAssetSettings settings = (AddressableAssetSettings)
-            AssetDatabase.LoadAssetAtPath(settingPath, typeof(AddressableAssetSettings));
-
-        //string t = AssetDatabase.AssetPathToGUID("Assets/models/elephant.fbx");
-        //settings.CreateOrMoveEntry(t, settings.DefaultGroup);
-
-        //List<AddressableAssetEntry> assetEntries = new List<AddressableAssetEntry>();
-        //settings.DefaultGroup.GatherAllAssets(assetEntries, true, false, false);
-
-
-        SwitchWebGL();
-        var webGlJson = settings.RemoteCatalogLoadPath + CleanAndBuildAssets();
-
-        SwitchAndroid();
-        var androidJson = settings.RemoteCatalogLoadPath + CleanAndBuildAssets();
-
-        AddressableAssetsConfigs configs = new AddressableAssetsConfigs();
-        configs.WebGL = webGlJson;
-        configs.Android = androidJson;
-
-
-        //List<AddressableAssetEntry> assetEntries = new List<AddressableAssetEntry>();
-        //AddressableAssetSettings.DefaultGroup
-        //AddressableAssetGroup.GatherAllAssets(assetEntries, true, false, false);
-    }*/
-
-    /// <summary>
-    /// Run a clean build before export.
-    /// </summary>
-    //[MenuItem("AddressableAsset/Clean&Build Assets")]
-    static private string CleanAndBuildAssets()
-    {
         string assetPath = Application.dataPath;
-        var bundlePath = assetPath.Replace("/Assets", "") + "/ServerData";
         string target = EditorUserBuildSettings.activeBuildTarget.ToString();
+        var bundlePath = assetPath.Replace("/Assets", "") + "/" + DataRoot;
         bundlePath = bundlePath + "/" + target;
-        //Debug.Log("bundlePath: " + bundlePath);
+        Debug.Log("bundlePath: " + bundlePath);
+
 
         try
         {
@@ -147,7 +159,7 @@ class BuildAddressablesProcessor
             AddressableAssetSettings.BuildPlayerContent();
             Debug.Log("BuildAddressablesProcessor.BuildAssets[" + target + "] done");
 
-            string[] files = System.IO.Directory.GetFiles(bundlePath, "*.json");
+            string[] files = Directory.GetFiles(bundlePath, "*.json");
 
             return Path.GetFileName(files[0]);
         }
@@ -156,6 +168,20 @@ class BuildAddressablesProcessor
             Debug.LogException(e);
             throw;
         }
+    }
+
+    [MenuItem("Switch Platform/WebGL")]
+    static void SwitchWebGL()
+    {
+        // Switch to WebGLbuild.
+        EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup.WebGL, BuildTarget.WebGL);
+    }
+
+    [MenuItem("Switch Platform/Android")]
+    static void SwitchAndroid()
+    {
+        // Switch to WebGLbuild.
+        EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup.Android, BuildTarget.Android);
     }
 
     //[Serializable]
